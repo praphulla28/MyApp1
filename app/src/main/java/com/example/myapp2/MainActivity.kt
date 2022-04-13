14a9dbe941d5ab5097055f78882c251e37e4fc23
Praphulla.Mahendra <Praphulla.Mahendra@incture.com>
2022-04-13 17:10:43 +0530

    s3

--- app/src/main/java/com/example/myapp2/MainActivity.kt
+++ app/src/main/java/com/example/myapp2/MainActivity.kt
@@ -2,10 +2,14 @@ package com.example.myapp2
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
+    lateinit var submit:Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
     }
 }
